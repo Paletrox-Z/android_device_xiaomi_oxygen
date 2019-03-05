d0425af830f14549cf1a6813db11cad7b56f456e
Paletrox-Z <paletroxzpz@gmail.com>
2019-03-05 18:20:35 +0530

    Misc: Adding auto push script
    Applying miscellaneous changes. This message is added by auto push script!

---
+++ auto-push.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+commit_name=$1
+commit_desc=$2
+git add *
+if [ -z "$commit_name" ]; then
+	commit_name="Auto commit: Misc changes"
+fi
+if [ -z "$commit_desc" ]; then
+	commit_desc="Applying miscellaneous changes. This message is added by auto push script!"
+fi
+git commit -m "$commit_name" -m "$commit_desc"
+git push origin pex
