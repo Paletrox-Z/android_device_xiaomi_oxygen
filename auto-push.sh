c80ae6ddc2f525cbf9524d149af49baab10a6547
Paletrox-Z <paletroxzpz@gmail.com>
2019-11-12 07:56:47 +0530

    Branch: Switching to a new branch to keep track of version
    Applying miscellaneous changes. This message is added by auto push script!

--- auto-push.sh
+++ auto-push.sh
@@ -9,4 +9,4 @@ if [ -z "$commit_desc" ]; then
 	commit_desc="Applying miscellaneous changes. This message is added by auto push script!"
 fi
 git commit -m "$commit_name" -m "$commit_desc"
-git push origin pex
+git push origin pex-p
